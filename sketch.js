c23a85382c0e53172f7476a97f56749580b4db04
ArmaansPiano <71108324+ArmaansPiano@users.noreply.github.com>
2020-11-19 20:15:26 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var wall,bullet;
+var speed,thickness, weight;
+function setup() {
+  createCanvas(1600,400);
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83);
+  bullet = createSprite(50,200,50,5);
+  bullet.velocityX = speed;
+  bullet.shapeColor = "white";
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = "blue";
+}
+
+function draw() {
+  background("black");
+  if(wall.x - bullet.x < (bullet.width + wall.width)/2) {
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+    if(damage > 10) {
+      bullet.shapeColor = "red";
+    }
+    if(damage < 10) {
+      bullet.shapeColor = "green";
+    }
+  }  
+  drawSprites();
+}
